92d297e54a2cd99f46b11a138e12460cf6eb7032
Narsimha <Narsimha@192.168.31.199>
2024-09-06 21:19:40 +0530

    my project commit sucessfully

---
+++ Aop/src/main/java/com/narsoAop/model/Employee.java
@@ -0,0 +1,35 @@
+package com.narsoAop.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Employee 
+{
+	
+	
+	
+	public String empId;
+	public String firstName;
+	public String lastName;
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
+}
